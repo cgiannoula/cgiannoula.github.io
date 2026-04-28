8ceade60b10a6d74de16e450e66a926ed0ed14dd
Christina Giannoula <cgiannoula@mbk-25-0328.mpi-sws.org>
2026-04-28 16:20:49 +0200

    Add attribution to Styleshout for CEEVEE template

--- includes/footer.php
+++ includes/footer.php
@@ -6,6 +6,7 @@
     <a href="/pages/privacy.php">Privacy Policy</a>
   </p>
   <p><span class="text-muted">&copy; Christina Giannoula. All rights reserved.</span></p>
+  <p>Template by <a href="http://www.Styleshout.com">Styleshout</a></p>
 </div>
 
 <!-- JS (load after content) -->
